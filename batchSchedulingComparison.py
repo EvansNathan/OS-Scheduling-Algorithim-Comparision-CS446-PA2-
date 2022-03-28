f49aba91cd8ac9af5b1a9b346c72e45602d3740f
EvansNathan <66696433+EvansNathan@users.noreply.github.com>
2022-03-28 12:13:29 -0700

    Removed Commented lines Added heading

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -1,3 +1,7 @@
+# Author: Nathan Evans
+# Date: 28 Mar 2022
+# Purpose: Compares different OS scheduling algorithm
+
 import sys
 
 
@@ -12,7 +16,6 @@ class Process:
         self.turnaround_time = 0
         self.wait_time = 0
         self.already_ran = False
-        # print("Created Process")
 
 
 def sort_by_arrival(process):
@@ -29,10 +32,10 @@ def sort_by_priority(process):
 
 def first_come_first_served_sort(process_list):
     print("First Come First Served Sort")
+
     sorted_process_list = sorted(process_list, key=sort_by_arrival)
     time = 0
     for process in sorted_process_list:
-        # print ("RAN")
         if time < process.arrival_time:
             time = process.arrival_time
 
@@ -52,7 +55,6 @@ def shortest_job_first_sort(process_list, sjf_run_order):
     last_process_ticked = Process(0, 0, 0, 0)
 
     while process_ran < len(sorted_process_list):
-        # print ("LOOP")
         next_process = -1
         for process in sorted_process_list:
             if process.arrival_time <= time and not process.already_ran:
@@ -107,11 +109,8 @@ def priority_sort(process_list):
 
 
 def average_turnaround(sorted_process_list):
-    # print ("Average Turnaround")
-
     total_turnaround_time = 0
     for process in sorted_process_list:
-        # print ("RAN")
         process.turnaround_time = process.time_completed - process.arrival_time
         total_turnaround_time = process.turnaround_time + total_turnaround_time
 
@@ -120,11 +119,8 @@ def average_turnaround(sorted_process_list):
 
 
 def average_wait(sorted_process_list):
-    # print ("Average Wait")
-
     total_wait_time = 0
     for process in sorted_process_list:
-        # print("RAN")
         process.wait_time = process.turnaround_time - process.original_burst_time
         total_wait_time = total_wait_time + process.wait_time
 
@@ -133,31 +129,24 @@ def average_wait(sorted_process_list):
 
 
 def main():
-    # print ("Main")
     n = len(sys.argv)
 
-    # Checks for 3 arguments in command line
     if n != 3:
         print ("Incorrect format, please provide 3 arguments")
         quit()
 
-    batch_file_name = sys.argv[1]
     sort_type = sys.argv[2]
     average_turnaround_time = 0
     average_wait_time = 0
     process_list = []
-    sorted_process_list = []
     sjf_run_order = []
 
     try:
         with open(sys.argv[1]) as in_file:
-            # File Found
-            # print("File Found")
             for line in in_file:
                 process_values = line.split(", ")
                 process_list.append(Process(process_values[0], process_values[1], process_values[2], process_values[3]))
     except Exception:
-        # File Was NOT Found
         print("File Not Found. Try Again")
         quit()
 
